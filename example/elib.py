3f33e3f5466f5cefc81ba22984c914df794fb209
Michael Schmidt <Michael.Schmidt82@gmail.com>
2022-03-10 10:11:39 -0600

    initial commit

---
+++ example/elib.py
@@ -0,0 +1,19 @@
+"""
+docstring
+"""
+
+class ExampleLib:
+    """
+    docstring
+    """
+    def __init__(self):
+        """
+        docstring
+        """
+        print('loading')
+
+    def say_hello(self, name: str ='world'):
+        """
+        docstring
+        """
+        print(f'hello {name}')
